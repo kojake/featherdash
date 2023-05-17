4646d13bfe007375ba04cf78ac43aecd6c172dd7
kaito <takahashikaito@takahashikaito23.local>
2023-05-17 23:01:29 +0900

    first commit

---
+++ feather dash/feather_dashApp.swift
@@ -0,0 +1,17 @@
+//
+//  feather_dashApp.swift
+//  feather dash
+//
+//  Created by kaito on 2023/05/17.
+//
+
+import SwiftUI
+
+@main
+struct feather_dashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            home()
+        }
+    }
+}
---
+++ feather dash/home.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  feather dash
+//
+//  Created by kaito on 2023/05/17.
+//
+
+import SwiftUI
+
+struct home: View {
+    var body: some View {
+        VStack {
+            Text("featherdash").font(.system(size: 100, weight: .black, design: .default))
+            
+            HStack{
+                Spacer()
+                VStack{
+                    Button(action: {
+                        
+                    }) {
+                        Image("home_button_design")
+                    }
+                    Button(action: {
+                        
+                    }) {
+                        Image("home_button_design2")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct home_Previews: PreviewProvider {
+    static var previews: some View {
+        home()
+    }
+}
